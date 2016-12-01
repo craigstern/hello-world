378914a053c2102604c5461a8d580fb7e4657dbb
craigstern <sinisterdesign@gmail.com>
2016-12-01 13:43:38 -0600

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//as a public static class, HelloWorld is *always* accessible to any other script that we 
+//create; there's no need to instantiate it or make it a component of a gameobject!
+public static class HelloWorld {
+
+      //as a public static method of this class, we can easily call sayHello()
+      //from anywhere in our code with nothing more than 'HelloWorld.sayHello()'
+      public static void sayHello () {
+            
+            //'Debug.Log' is the bit that actually prints a string in the Unity console; 
+            //it's just a built-in Unity function that takes a string as an argument
+            Debug.Log( "hello world" );
+      }
+}
